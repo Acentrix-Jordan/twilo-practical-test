19d99b9f1f7634146c1063b448f3f508296bd973
Jordan Wilson <biggyj_dev@outlook.com>
2022-05-28 15:30:38 +0100

    Recaptcha added

--- src/Components/MailingList.js
+++ src/Components/MailingList.js
@@ -1,6 +1,9 @@
 import React from "react";
+import DB from "../data/data.json";
 
 const MailingList = () => {
+  const imageStyling = "max-w-60 max-h-60 p-2 box-border";
+
   return (
     <div className='p-8 bg-mail-main flex flex-col items-center'>
       <div className='relative items-center justify-center'>
@@ -13,25 +16,33 @@ const MailingList = () => {
           join the mailing list and send in your photos
         </h1>
       </div>
-      <form action='POST' className='flex flex-col items-center lg:flex-row'>
+      <form
+        name='mailing list'
+        action='POST'
+        data-netlify='true'
+        className='flex flex-col items-center lg:flex-row'
+      >
         <input
           type='text'
           placeholder='*Name'
+          id='name'
           required
           className='border-2 border-yellow p-2 m-2 w-80'
         />
         <input
           type='email'
+          id='email'
           placeholder='*Email'
           required
           className='border-2 border-yellow p-2 m-2 w-80'
         />
+        <div data-netlify-recaptcha='true'></div>
         <div className='relative w-32 flex items-center justify-center m-4'>
           <img src='./images/mail_decoration.png' alt='' className='absolute' />
           <input
             type='submit'
             value='SUBSCRIBE'
-            className='font-bold hover:text-yellow z-40'
+            className='font-bold hover:text-yellow z-40 hover:transition hover:duration-300'
           />
         </div>
       </form>
@@ -40,31 +51,18 @@ const MailingList = () => {
         id='images'
         className='hidden md:flex justify-center overflow-hidden w-full p-4 h-[300px]'
       >
-        <img
-          src='./images/dummy_image4.png'
-          alt=''
-          className='max-w-60 max-h-60 p-2 box-border hidden lg:block'
-        />
-        <img
-          src='./images/dummy_image5.png'
-          alt=''
-          className='max-w-60 max-h-60 p-2 box-border '
-        />
-        <img
-          src='./images/dummy_image6.png'
-          alt=''
-          className='max-w-60 max-h-60 p-2 box-border '
-        />
-        <img
-          src='./images/dummy_image7.png'
-          alt=''
-          className='max-w-60 max-h-60 p-2 box-border '
-        />
-        <img
-          src='./images/dummy_image8.png'
-          alt=''
-          className='max-w-60 max-h-60 p-2 box-border hidden lg:block'
-        />
+        {DB.mailingListImages.map((image) => {
+          return (
+            <img
+              id={image.id}
+              src={image.image}
+              alt={image.imageDescription}
+              className={
+                image.id < 4 ? imageStyling : `${imageStyling} hidden lg:block`
+              }
+            />
+          );
+        })}
       </div>
     </div>
   );
